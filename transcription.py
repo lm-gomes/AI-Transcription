cf161b071e2a61a3a6632524e2f982c49e800977
lm-gomes <lm-gomes>
2024-09-03 22:46:18 -0300

    pdf transcription first version

--- transcription.py
+++ transcription.py
@@ -3,11 +3,31 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 import whisper
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
 
 model = whisper.load_model("base")
 filepath = ""
 filedest = ""
 
+def text_to_pdf(text, output_filename):
+    pdf = canvas.Canvas(output_filename, pagesize=letter)
+
+    pdf.setFont("Helvetica", 12)
+    width, height = letter
+    y_position = height - 40
+    lines = text.splitlines()
+
+    for line in lines:
+        if y_position < 40:
+            pdf.showPage()
+            pdf.setFont("Helvetica", 12)
+            y_position = height - 40
+        pdf.drawString(40, y_position, line)
+        y_position -= 14
+
+    pdf.save()
 def openDestination():
     global filedest
     filedest = filedialog.askdirectory()
@@ -22,13 +42,20 @@ def openDirectory():
 def transcribe_audio(audio_path):
     result = model.transcribe(audio_path)
     return result['text']
-def transcribe():
+def transcribe_text(doc_type):
     global filepath
     print("This is the filepath: " + filepath)
     audio_path = filepath
     transcribed_text = transcribe_audio(audio_path)
     file = open(filedest + "/transcription.txt", "w")
     file.write(transcribed_text)
+
+def transcribe_pdf():
+    global filepath
+    print("This is the filepath: " + filepath)
+    audio_path = filepath
+    transcribed_text = transcribe_audio(audio_path)
+    text_to_pdf(transcribed_text, filedest + "/transcription_output.pdf")
 
 
 window = Tk()
@@ -45,6 +72,6 @@ button.grid(column=2, row=1, sticky=(N))
 
 destination = ttk.Label(mainframe, text="Chose the transcription destination: ").grid(column=1, row=3, sticky=N)
 destbutton = ttk.Button(mainframe, text="Destination", command=openDestination).grid(column=2, row=3, sticky=N)
-transcribebutton = ttk.Button(mainframe, text="Transcribe", command=transcribe).grid(column=2, row=4, sticky=N)
-
+transcribetext_button = ttk.Button(mainframe, text="Transcribe to Text", command=transcribe_text).grid(column=1, row=4, sticky=N)
+transcribepdf_button = ttk.Button(mainframe, text="Transcribe to PDF", command=transcribe_pdf).grid(column=2, row=4, sticky=N)
 window.mainloop()
